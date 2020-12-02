9e54877565529a03ab9936de7cda97e6c07c49bb
Ventsislav Tsochev <ventsi@limechain.tech>
2020-12-02 13:31:38 +0200

    caped token

--- token-contract/deployment/deploy.js
+++ token-contract/deployment/deploy.js
@@ -1,11 +1,18 @@
 const etherlime = require("etherlime-lib");
+const ehters = require("ethers");
 const ReMCToken = require("../build/ReMCToken.json");
 const TokenTimelock = require("../build/TokenTimelock.json");
 
+const MerkleUtils = require('../build/MerkleUtils.json');
+const ConvertUtils = require('../build/ConvertUtils.json');
+const MerkleAirDrop = require('../build/MerkleAirDrop.json');
+
 const INFURA_PROVIDER = "148dda379bdd4346ae1ad2e9a159249d";
 const ETHERSCAN_API_KEY = "J531BRU4FNGMNCD693FT6YS9TAM9TWS6QG";
-const MINTER_ADDRESS = "0x4555A429Df5Cc32efa46BCb1412a3CD7Bf14b381";
 const ETHERSCAN = "etherscan";
+
+const TOKEN_DISTRIBUTOR_ADDRESS = "0x39F00e926DeE09De7f44646a640e83fd912Bec17";
+const PREMINTED_AMOUNT = ethers.utils.parseEther("1000000000")
 
 const name = "ReMeLifeCore";
 const symbol = "ReMC";
@@ -26,7 +33,8 @@ const deploy = async (network, secret) => {
         {},
         name,
         symbol,
-        MINTER_ADDRESS
+        TOKEN_DISTRIBUTOR_ADDRESS,
+        PREMINTED_AMOUNT
     );
 
     const tokenTimelockInstance = await deployer.deployAndVerify(
@@ -36,8 +44,21 @@ const deploy = async (network, secret) => {
         reMCTokenInstance.contractAddress,
         maxWithdrawDeposits
     );
+
+    const MerkleUtilsLib = await deployer.deploy(MerkleUtils);
+    const ConvertUtilsLib = await deployer.deploy(ConvertUtils);
+    const MerkleAirDropDeployed = await deployer.deployAndVerify(
+        ETHERSCAN,
+        MerkleAirDrop, {
+        MerkleUtils: MerkleUtilsLib.contractAddress,
+        ConvertUtils: ConvertUtilsLib.contractAddress
+    }, reMCTokenInstance.contractAddress, TOKEN_DISTRIBUTOR_ADDRESS);
+
 };
 
 module.exports = {
     deploy,
 };
+
+
+// etherlime deploy --network rinkeby --secret 0xc44377c50845ee070c98c4295bde42f2fbf5a91e81ce2d6d23683291f23dfe0a --compile false
